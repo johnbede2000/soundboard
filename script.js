e978645d242da8ce3abd08cc3de117ab381b1ec5
John Bede <johnbede2000@gmail.com>
2020-09-05 22:54:45 +0100

    face change

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+const face = document.querySelector('.img');
+const kbds = document.querySelectorAll('kbd');
+
 function johnPiano(e) {
     const sing = document.querySelector(`audio[data-eventKey='${e.keyCode}']`);
     const key = document.querySelector(`kbd[data-eventKey='${e.keyCode}']`);
+
     if (sing==null) {
         return;
     }
     sing.currentTime = 0;
     sing.play();
     key.classList.add('singing');
+    face.classList.add('open');
 }
 
 window.addEventListener('keydown', johnPiano);
-
-const kbds = document.querySelectorAll('kbd');
-kbds.forEach(listenTranEnd);
 
 function listenTranEnd(currentValue) {
     currentValue.addEventListener('transitionend', removeTransition);
@@ -21,5 +23,8 @@ function listenTranEnd(currentValue) {
 function removeTransition(e) {
     if (e.propertyName == 'transform') {
         this.classList.remove('singing');
+        face.classList.remove('open');
     }
 }
+
+kbds.forEach(listenTranEnd);
